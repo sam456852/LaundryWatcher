94000614ac77cb57cafaa0cc7534ca809c8602a0
Tony Cao <yzcaoye@163.com>
2017-05-30 00:38:42 -0500

    local notification

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+// import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
 import { StatusService } from '../../providers/status-service';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [StatusService]
+  providers: [StatusService, LocalNotifications]
 })
 export class HomePage {
 
@@ -20,7 +23,11 @@ export class HomePage {
   dryer_start_time_list: string[];
   dryer_remaining_time_list: string[];
 
-  constructor(public navCtrl: NavController, private statusService: StatusService) {
+  notifications: any[] = [];
+
+  constructor(public navCtrl: NavController, private statusService: StatusService, public platform: Platform, public alertCtrl: AlertController, private localNotifications: LocalNotifications) {
+    console.log("moment: "+moment(new Date()).format());
+    console.log("minutes: "+ new Date().getMinutes());
     this.washing_status_list = [];
     this.dryer_status_list = [];
 
@@ -30,7 +37,74 @@ export class HomePage {
     this.dryer_remaining_time_list = [];
 
     this.load();
-    console.log("status: "+this.washing_status_list);
+    // console.log("status: "+this.washing_status_list);
+
+    // this.addNotifications();
+    // console.log("Notifications to be scheduled: ", this.notifications);
+
+  }
+
+  addNotifications(name, time){
+    let notification_time = new Date(new Date().getTime() + time*6*1000);
+    // notification_time.setHours(new Date().getHours());
+    // notification_time.setMinutes(new Date().getMinutes()+time);
+    let notification = {
+      // id: day.dayCode,
+      title: 'laundrywatcher',
+      text: name+' is available now!',
+      at: notification_time,
+      icon: 'http://example.com/icon.png'
+      // every: 'week'
+    };
+    console.log("notification_time: "+notification_time);
+    this.notifications.push(notification);
+  }
+
+  setNotificiation(name, remaining_time){
+    if(this.platform.is('cordova')){
+            // Cancel any existing notifications
+            // this.localNotifications.cancelAll().then(() => {
+        //
+                // Schedule the new notifications
+                this.localNotifications.schedule(this.notifications);
+                // this.notifications = [];
+        //
+                let alert = this.alertCtrl.create({
+                    title: name,
+                    message:'You will get notified after '+remaining_time +' minutes',
+                    buttons: ['Ok']
+                });
+        //
+                alert.present();
+        //
+            // });
+        //
+        }
+        else{
+          let alert = this.alertCtrl.create({
+              title: 'something wrong!',
+              buttons: ['Ok']
+          });
+    //
+          alert.present();
+        }
+  }
+
+  getWashingNotified(washing_name){
+    // console.log("washing notification: "+washing_name);
+    this.statusService.getWashingRemainingTimeByName(washing_name).then((remaining_time) => {
+      // console.log("washing remaining_time: "+washing_name+" "+remaining_time);
+      this.addNotifications(washing_name, remaining_time);
+      this.setNotificiation(washing_name, remaining_time);
+    })
+  }
+
+  getDryerNotified(dryer_name){
+    // console.log("dryer notification: "+dryer_name);
+    this.statusService.getDryerRemainingTimeByName(dryer_name).then((remaining_time) => {
+      this.addNotifications(dryer_name, remaining_time);
+      this.setNotificiation(dryer_name, remaining_time);
+    })
   }
 
   load(){
